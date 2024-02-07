0e3bf257c10c53775163eabcd4380c035e718cdb
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-02-07 00:25:53 -0300

    feat: Header component

---
+++ src/screens/Home/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import { colors } from "@/styles/global";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  headerContainer: {
+    width: "100%",
+    height: 173,
+    backgroundColor: colors["gray-700"],
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
