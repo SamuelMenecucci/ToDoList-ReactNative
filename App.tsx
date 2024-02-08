b8be98fb688ffe81788b704864e8a7d494f717f6
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-02-07 22:26:16 -0300

    chore: added and config external font

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
 import { Home } from "@/screens/Home";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
 import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <StatusBar style="auto" />
